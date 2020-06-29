4143de33fa2ca9fc4526deddf89116976900ae20
Samarth <sam.sajwan.2000@gmail.com>
2020-06-29 20:24:44 +0530

    new files added.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,9 +12,24 @@ class UsersController < ApplicationController
 
 	def show
 		@users=User.all
-		
+
 	end
 
+	def delete
+		@user=User.find(params[:id])
+		@user.delete
+
+		redirect_to show_user_path
+
+	end
+
+	def edit
+		@user = User.find(params[:id])
+  		unless @user
+    		flash[:error] = "User not found"
+    		redirect_to root_path
+  end
+	end
 
 	def create
 		@user=User.new(params.require(:user).permit(:name, :email, :phone))
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
 root 'users#index'
-
+get 'show' => 'users#show'
 get 'about' => 'pages#about'
+get 'edit' =>'users#edit'
+get 'delete' => 'users#delete'
 
 	resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
